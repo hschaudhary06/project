4359a19c7b943d3c6cd4bd17643f40de4f075002
root <root@ip-172-31-35-168.ap-south-1.compute.internal>
2023-08-24 12:25:52 +0000

    create task_1

---
+++ task_1.py
@@ -0,0 +1,50 @@
+print("Python Calculator")
+print("-----------------")
+
+def calculater():
+	num_1 = int(input("Enter your first number : "))
+	num_2 = int(input("Enter your second number : "))
+
+	opretion = input(''' Please type in the math operation you would like to complate :
+			+ for addition
+			- for substraction
+			* for multiplication
+			/ for division
+			** for power
+			% for modulo
+			: ''')
+	if opretion == '+':
+		print('{} + {} = '.format(num_1,num_2))
+		print(num_1+num_2)
+	elif opretion == '-':
+		print('{} - {} = '.format(num_1,num_2))
+		print(num_1 - num_2)
+	elif opretion == '*':
+		print('{} * {} = '.format(num_1,num_2))
+		print(num_1 * num_2)
+	elif opretion == '/':
+		print('{} + {} = '.format(num_1,num_2))
+		print(num_1 / num_2)
+	elif opretion == '**':
+		print('{} ^ {}'.format(num_1,num_2))
+		print(num1_ ** num_2)
+	elif opretion == '%':
+		print('{} % {} = '.format(num_1,num_2))
+		print(num_1 % num_2)
+	else:
+		print("Please Enter valid opretion!")
+
+calculater()
+
+def again():
+	responce = input(''' Do you want to calculate again ? 
+		  if yes then Type 'Y' or no then Type 'N' :
+		''')
+	if responce.upper() == 'Y':
+		calculater()
+		again()
+	elif responce.upper() == 'N':
+		print("see you again :).")
+	else:
+		again()
+again()
